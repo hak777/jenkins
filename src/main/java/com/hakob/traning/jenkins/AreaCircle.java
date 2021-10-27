ad8b679ee576325cd5d7bab3ce8d8ef3a69dfca1
hak777 <hak777@live.ru>
2021-10-27 09:24:41 +0400

    Your message about the commit

---
+++ src/main/java/com/hakob/traning/jenkins/AreaCircle.java
@@ -0,0 +1,11 @@
+package com.hakob.traning.jenkins;
+
+public class AreaCircle {
+
+	public double area (double  radius )
+	{
+		return Math.PI * radius * radius;
+	}
+
+}
+ 
---
+++ src/test/java/com/hakob/traning/jenkins/JenkinsTest.java
@@ -0,0 +1,12 @@
+package com.hakob.traning.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsTest {
+	@Test
+	public void maintest() {
+	assertNotNull("optional failure message");
+	}
+}
